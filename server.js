69e5eae087a986d905706e3323cd6081fb05d01e
Gloria Guy <totallygloria@gmail.com>
2016-11-10 16:08:42 -0800

    Added the package.json for the project, and a basic server file. License is the Creative Commons non-commercial.

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const app = require('./lib/app');
+const port = process.env.PORT || 3000;
+require('./lib/setup-mongoose');
+
+const server = http.createServer(app);
+
+server.listen(port, err => {
+  if (err) {
+    console.error('Error starting the server: ', err);
+  } else {
+    console.log('Server listening on port ', port);
+  }
+});
