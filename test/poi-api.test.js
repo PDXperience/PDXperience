33a90c267476664b8d6a66fb0ff40ee86af5901b
Nathan Hugon <nthugon@gmail.com>
2016-11-13 15:49:23 -0800

    setup poi e2e test page creating user and sample poi

---
+++ test/poi-api.test.js
@@ -0,0 +1,42 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const assert = chai.assert;
+chai.use(chaiHttp);
+
+const connection = require('../lib/setup-mongoose');
+
+const app = require('../lib/app');
+
+describe('Poi:', () => {
+
+  before( done => {
+    const drop = () => connection.db.dropDatabase(done);
+    if (connection.readyState === 1) drop();
+    else connection.on( 'open', drop );
+  });
+
+  const request = chai.request(app);
+  let token = '';
+
+  before(done => {
+    request
+      .post('/api/auth/signup')
+      .send({email:'somebody@somebody.com', password:'password', firstName: 'first-name', admin: true})
+      .then(res => {
+        assert.ok(res.body.token);
+        token = res.body.token;
+      })
+      .then(done)
+      .catch(done);
+  });
+
+  const somePark = {
+    name: 'some park',
+    type: 'park',
+    location: '123 some st',
+    hours: 'dawn to dusk'
+  };
+
+ 
+
+});
