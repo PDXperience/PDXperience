dffaf8d17a5ce98dc7a57389a49f29a5c67fffd4
Gloria Guy <totallygloria@gmail.com>
2016-11-16 17:40:06 -0800

    New users can now sign up and get a token back, which is saved in local storage.

--- public/scripts/controllers/userController.js
+++ public/scripts/controllers/userController.js
@@ -22,10 +22,38 @@
       console.log(err);
     })
     .done(res => {
-      localStorage.setItem('token', res.body.token);
+      localStorage.setItem('token', res.token);
       console.log(res);
     });
+  });
 
+  $('#signup-form').on('submit', function(event) {
+    console.log('clicked submit button');
+    event.preventDefault();
+    var password= $('#POST-signup-password').val();
+    var email = $('#POST-signup-email').val();
+    var firstName = $('#POST-signup-firstName').val();
+
+    $.ajax({
+      method:'POST',
+      url: '/api/auth/signup',
+      headers: {
+        'content-type': 'application/json'
+      },
+      data: JSON.stringify({
+        'email': email,
+        'password': password,
+        'firstName': firstName
+      })
+    })
+      .fail(err => {
+        $('#signup-response').append(err.responseText);
+        console.log(err);
+      })
+      .done(res => {
+        localStorage.setItem('token', res.token);
+        console.log(res);
+      });
   });
 
   var userController = {};
